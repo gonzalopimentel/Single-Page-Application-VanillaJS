51c33988c63ec9a95fc0171166638b893645a7b6
gonzalopimentel <gonzalo.pimentel.ramirez@gmail.com>
2020-06-21 12:23:07 -0500

    Commit Full Project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/*Nos permite acceder hacia donde estamos dentro de las carpetas o hacia donde nos movemos*/
+const path = require("path");
+/*Archivo para trabajar con HTML */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/styles/styles.css", to: "" }],
+    }),
+  ],
+};
