fa5b14aeecf5a443447a798a7bf08c4aecb450e3
gonzalopimentel <gonzalo.pimentel.ramirez@gmail.com>
2020-06-27 12:10:04 -0500

    Las fixing Home anchor

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/Single-Page-Application-VanillaJS/#/3/">
+          <a href="/Single-Page-Application-VanillaJS/">
             Home
           </a>
         </h1>
